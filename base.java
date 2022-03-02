3d15f1730be5713410e79d379fa7ee0c60300cdd
JC <71771862+gls16@users.noreply.github.com>
2022-03-02 14:41:59 -0500

    Create base.java

---
+++ base.java
@@ -0,0 +1,29 @@
+public class NewClass {
+    public static void main (String args[]){
+        //Definir una variable
+        
+        int myvariable = 10;
+        System.out.println(myvariable);
+        
+        //modificar el valor de la variable
+        myvariable = 5;
+        System.out.println(myvariable);
+        
+        //variable de tipo texto
+        String myVariableTexto = "hola";
+        System.out.println(myVariableTexto);
+        
+        //var -> inferencina de tipos en Java
+        var myvariablenueva = 12;
+        System.out.println(myvariablenueva);
+        
+        var myVariableTexto2 = "hola";
+        System.out.println(myVariableTexto2);
+        
+        
+        /*
+        sout = System.out.println();
+        soutv = System.out.println("myVariableTexto2 = " + myVariableTexto2);
+        */
+    }
+}
